3a270814de623be6b075102aeaaaa21bb6bb4176
John Ky <newhoggy@gmail.com>
2018-07-24 22:07:20 +1000

    simple-json example

--- simple-json/app/Main.hs
+++ simple-json/app/Main.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main where
 
-import Lib
+import Control.Monad
+import Data.Aeson
+import GHC.Stats
+import System.Posix.Process
+import System.Process
+
+import qualified Data.ByteString.Lazy as BS
+import qualified System.Environment   as IO
 
 main :: IO ()
-main = someFunc
+main = do
+  pid <- getProcessID
+  (filename:_) <- IO.getArgs
+  bs <- BS.readFile filename
+  let !maybeJson = decode bs :: Maybe Value
+
+  system $ "ps aux | grep " <> show pid <> " | grep -v grep"
+
+  forM_ maybeJson $ \_ ->
+    putStrLn "Done"
