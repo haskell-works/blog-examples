ff900fea6d585142285f565c0cdf7af2af1712f2
John Ky <newhoggy@gmail.com>
2019-04-08 08:50:11 +1000

    Fix hlint annotation

--- ex-vector/test/Ops/SumBitVectorsSpec.hs
+++ ex-vector/test/Ops/SumBitVectorsSpec.hs
@@ -33,7 +33,7 @@ import qualified Data.Vector.Storable as DVS
 
 {-# ANN module ("HLint: ignore Redundant do"        :: String) #-}
 {-# ANN module ("HLint: ignore Reduce duplication"  :: String) #-}
-{-# ANN module ("HLint: redundant bracket"          :: String) #-}
+{-# ANN module ("HLint: ignore Redundant bracket"   :: String) #-}
 
 spec :: Spec
 spec = describe "HaskellWorks.Data.Json.ValueSpec" $ do
